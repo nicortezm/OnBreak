cc4a185886eb77daa97a35c6e99085849e90af2f
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2020-04-21 19:28:50 -0400

    Clase Contrato agregada

---
+++ RegistroClientes/BibliotecaClientes/Contrato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Contrato
+{
+    public int NumContrato { get; set; }
+    public Contrato()
+	{
+        this.init();
+	}
+    public void init()
+    {
+        NumContrato = 0;
+    }
+}
